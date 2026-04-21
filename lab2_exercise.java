9cf63fe18d5870cea5f703a132d910c3eef5dfdc
Owais Hashim <owaishashim41@gmail.com>
2026-04-21 10:33:48 +0500

    updates

--- lab2_exercise.java
+++ lab2_exercise.java
@@ -6,22 +6,30 @@ class student{
         this.stdName = stdName;
         this.RegNo = RegNo;
         this.cgpa = cgpa;
-    }}
+    }
+    float maximum(student[] students){
+        float maxCgpa = students[0].cgpa;
+        for(int i=1; i<students.length;i++){
+            if(students[i].cgpa > maxCgpa){
+                maxCgpa = students[i].cgpa;
+            }
+        }
+        return maxCgpa;
+    }    
+}
 public class lab2_exercise {
     public static void main(String[] args) {
-         /*  student[] students = new student[5];
+       student[] students = new student[5];
         students[0] = new student("Alice", "REG001", 3.5f);
         students[1] = new student("Bob", "REG002", 3.8f);
         students[2] = new student("Charlie", "REG003", 3.2f);
         students[3] = new student("David", "REG004", 3.9f);
         students[4] = new student("Eve", "REG005", 3.6f);
-        float greater = students[0].cgpa;
-        for(int i = 0; i < students.length; i++){
-            if(students[i].cgpa > greater){
-                greater = students[i].cgpa; }} 
-            System.out.println("The highest CGPA is: " + greater);*/
-        
-    int[][] matrix = {
+
+        float greater = students[0].maximum(students);
+        System.out.println("The highest CGPA is: " + greater);
+
+   /*  int[][] matrix = {
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9}
@@ -31,7 +39,7 @@ public class lab2_exercise {
     for (int i = 0; i < matrix.length; i++) {
         sum += matrix[i][i]; // main diagonal
     }
-      System.out.println("Sum of main diagonal: " + sum);
+      System.out.println("Sum of main diagonal: " + sum);*/
 }} 
     
     
